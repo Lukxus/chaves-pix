17a785da5c295a777df4cc1ef50069e7e962e1b4
Luiz Saraiva <luizsaraiva50@gmail.com>
2025-02-05 21:47:27 -0300

    Primeiro commit: projeto chaves

---
+++ src/main/java/com/pix/chaves/ChavesApplication.java
@@ -0,0 +1,13 @@
+package com.pix.chaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChavesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pix/chaves/ChavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pix.chaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
