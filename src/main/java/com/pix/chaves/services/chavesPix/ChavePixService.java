a62b1c0f350ebe318bff70bad82f1057e88964f5
Luiz Saraiva <luizsaraiva50@gmail.com>
2025-02-11 08:44:21 -0300

    Ajuste imports

--- src/main/java/com/pix/chaves/services/chavesPix/ChavePixService.java
+++ src/main/java/com/pix/chaves/services/chavesPix/ChavePixService.java
@@ -1,6 +1,5 @@
 package com.pix.chaves.services.chavesPix;
 
-import com.pix.chaves.domain.enums.TipoChave;
 import com.pix.chaves.mapper.ChavePixMapper;
 import com.pix.chaves.rest.dto.request.ChavePixFilter;
 import com.pix.chaves.rest.dto.request.CreateChavePixRequest;
@@ -12,8 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @Service
